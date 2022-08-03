e98d701e30141049d31336e2bac05cf6e7ff8ffb
yevhenpodoliaka <93725802+yevhenpodoliaka@users.noreply.github.com>
2022-08-03 07:57:21 +0000

    Deploying to gh-pages from @ yevhenpodoliaka/goit-react-hw-05-movies@def374da197589102b787437a2731292515370a2 🚀

---
+++ static/js/805.6c7d2bf5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(501),o=t(184);function c(){return(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("h1",{children:"404  NotFound !!!"}),(0,o.jsx)(r.rU,{to:"/",children:"Go Home Page"})]})}}}]);
+//# sourceMappingURL=805.6c7d2bf5.chunk.js.map
